bd9f52ba2c9631cd254726ac9df05646d7a0c49c
Moses Cyabukombe <mosescy7@hotmail.com>
2026-07-08 13:00:32 +0200

    Correct business address and map pin to 35a KG 37 Avenue
    The address and map embed previously pointed to a generic Kimironko
    area search rather than the actual property location.

--- build.py
+++ build.py
@@ -6,11 +6,11 @@ OUT = os.path.dirname(os.path.abspath(__file__))
 WA = "250788750184"
 PHONE = "+250 788 750 184"
 EMAIL = "cftrading26@gmail.com"
-ADDRESS_LINE1 = "6-12 KG 150 St"
+ADDRESS_LINE1 = "35a KG 37 Avenue"
 ADDRESS_LINE2 = "Kimironko, Gasabo District, Kigali, Rwanda"
 AIRBNB1 = "https://www.airbnb.com/rooms/1710869845463366565"
 AIRBNB2 = "https://www.airbnb.com/rooms/14240039"
-MAP = ("https://maps.google.com/maps?q=Kimironko%2C%20Kigali%2C%20Rwanda&t=&z=15&ie=UTF8&iwloc=&output=embed")
+MAP = ("https://maps.google.com/maps?q=-1.945522,30.1362201&t=&z=16&ie=UTF8&iwloc=&output=embed")
 
 WA_SVG = '<svg viewBox="0 0 32 32" aria-hidden="true"><path d="M16 3C9.4 3 4 8.4 4 15c0 2.6.8 5 2.3 7L4 29l7.2-2.3c1.9 1 4 1.6 6.3 1.6 6.6 0 12-5.4 12-12S22.6 3 16 3zm0 21.8c-2 0-3.9-.6-5.5-1.5l-.4-.2-4.3 1.4 1.4-4.1-.3-.4A9.7 9.7 0 0 1 6.2 15c0-5.4 4.4-9.8 9.8-9.8s9.8 4.4 9.8 9.8-4.4 9.8-9.8 9.8zm5.4-7.3c-.3-.2-1.8-.9-2-1s-.5-.2-.7.2-.8 1-1 1.2-.4.2-.7 0a8 8 0 0 1-2.4-1.5 9 9 0 0 1-1.6-2c-.2-.3 0-.5.1-.7l.5-.5.3-.5c.1-.2 0-.4 0-.6l-.9-2.2c-.2-.6-.5-.5-.7-.5h-.6c-.2 0-.5 0-.8.4-.3.3-1 1-1 2.5s1 2.9 1.2 3.1c.2.2 2.1 3.2 5.1 4.5.7.3 1.3.5 1.7.6.7.2 1.4.2 1.9.1.6-.1 1.8-.7 2-1.4.3-.7.3-1.3.2-1.4l-.6-.3z"/></svg>'
 
